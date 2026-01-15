efa8b2ce8cfcdaa7fec87f74bef8b6240163c1e2
TeamDman <TeamDman9201@gmail.com>
2026-01-15 11:29:49 -0500

    add paycheque show impl, failing due to
    https://github.com/facet-rs/facet/issues/1791

---
+++ src/paycheque/paycheque_show_request.rs
@@ -0,0 +1,44 @@
+use crate::net::CacheKey;
+use crate::net::ClientExt;
+use crate::net::HasCacheKey;
+use crate::net::create_client;
+use crate::paycheque::ChequeNumber;
+use crate::paycheque::PaychequeShowResponse;
+use std::path::PathBuf;
+use std::pin::Pin;
+
+pub struct PaychequeShowRequest {
+    pub cheque_number: ChequeNumber,
+}
+
+impl HasCacheKey for PaychequeShowRequest {
+    fn cache_key(&self) -> CacheKey {
+        CacheKey::new(PathBuf::from_iter([
+            "paycheque",
+            "show",
+            self.cheque_number.as_ref(),
+        ]))
+    }
+}
+
+impl IntoFuture for PaychequeShowRequest {
+    type Output = eyre::Result<PaychequeShowResponse>;
+    type IntoFuture = Pin<Box<dyn Future<Output = Self::Output>>>;
+
+    fn into_future(self) -> Self::IntoFuture {
+        Box::pin(async move {
+            let url = "https://mapayegc-mygcpay.tpsgc-pwgsc.gc.ca/en/mygcpay/paycheque-data/";
+
+            let client = create_client().await?;
+            let req = client
+                .post(url)
+                .form(&[
+                    ("type", "buildDetailedTables"),
+                    ("chkNumber", &self.cheque_number),
+                ])
+                .build()?;
+            let resp = client.execute_cached(req, &self.cache_key()).await?;
+            Ok(resp)
+        })
+    }
+}
